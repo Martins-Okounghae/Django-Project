a755c01b809013a549816405feb652c80a4e1239
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2019-09-01 03:58:13 +0100

    updated views page

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
-from django.http import HttpResponse
-from django.core.urlresolvers import reverse_lazy
+
 from django.urls import reverse
 
 from django.core.mail import send_mail, BadHeaderError
